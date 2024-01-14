8d4ca0b90bb5dd828a3230c25102227570f9fa46
Rūta <gec.ruta@gmail.com>
2024-01-15 00:06:33 +0200

    url modification

--- src/utils/Url.js
+++ src/utils/Url.js
@@ -1,6 +1,8 @@
-const API_LOCAL = "http://localhost:5160/api"
-const API_REMOTE = "https://coral-app-4hvmu.ondigitalocean.app/api"
+// const API_LOCAL = "http://localhost:5160/api"
+// const API_REMOTE = "https://coral-app-4hvmu.ondigitalocean.app/api"
 
-const URL = process.env.NODE_ENV === 'development' ? API_LOCAL : API_REMOTE;
+// const URL = process.env.NODE_ENV === 'development' ? API_LOCAL : API_REMOTE;
+
+const URL = "https://coral-app-4hvmu.ondigitalocean.app/api"
 
 export default URL;
